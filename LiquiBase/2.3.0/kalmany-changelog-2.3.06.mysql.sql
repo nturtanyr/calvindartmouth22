ec397f95b5f77de5c59ca1e48bd3a773193d53e7
nturtanyr <nturnet@gmail.com>
2022-05-24 16:08:15 +0100

    Added relationship states

---
+++ LiquiBase/2.3.0/kalmany-changelog-2.3.06.mysql.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset Stanley:02030406 labels:v2.3.0
+INSERT INTO `relationships_state` (`name`) VALUES ('Dating');
+INSERT INTO `relationships_state` (`name`) VALUES ('Cohabiting');
+INSERT INTO `relationships_state` (`name`) VALUES ('Married');
+INSERT INTO `relationships_state` (`name`) VALUES ('Broke Up');
+INSERT INTO `relationships_state` (`name`) VALUES ('Divorced');
+INSERT INTO `relationships_state` (`name`) VALUES ('Widowed');
+INSERT INTO `relationships_state` (`name`) VALUES ('Afterlife');
